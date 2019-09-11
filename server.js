db0b9e80015a8b3c2a5a4b96369266e7ecc5e765
ujwaldeeps <ujwaldeeps@gmail.com>
2019-09-11 05:57:49 -0400

    updated to deploy to heroku

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/plan-validations'));
+app.use(express.static(__dirname + '/dist/library'));
 
 app.get('/*', function(req,res) {
 
-    res.sendFile(path.join(__dirname+'/dist/plan-validations/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/library/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
